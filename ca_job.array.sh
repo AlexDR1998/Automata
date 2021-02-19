a35b839025a5c96e9090c5836778bf046448bd0e
Alex <alexander.richardson@gmail.com>
2021-02-19 15:19:15 +0000

    fixed stupid bash script bug

--- ca_job.array.sh
+++ ca_job.array.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #$ -N ca_run
 #$ -cwd
-#$ -l h_rt=100:00:00
+#$ -l h_rt=20:00:00
 #$ -l h_vmem=4G
-ca_job.sh $SGE_TASK_ID
+bash ca_job.sh $SGE_TASK_ID
--- ca_job.sh
+++ ca_job.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #$ -N ca_run
 #$ -cwd
-#$ -l h_rt=100:00:00
+#$ -l h_rt=20:00:00
 #$ -l h_vmem=4G
 
 module load python/3.4.3
